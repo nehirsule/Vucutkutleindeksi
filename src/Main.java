97d5756b79bb15fd065dcc21a4d063a67bccaea1
Şule Nehir ARAÇ <147002677+nehirsule@users.noreply.github.com>
2024-02-22 11:18:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //kilo, boy tanımlayalım. Double ile çünkü tam sayı olmayabilir.
+        double kilo ,boy , bmi;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kilonuzu kilogram cinsinden giriniz : ");
+        // Kullanıcıdan kilo bilgisi alıyoruz
+        kilo = input.nextDouble();
+
+        System.out.print("Boyunuzu metre cinsinde giriniz(örn:1,65) : ");
+        // Kullanıcıdan kilo bilgisi alıyoruz
+        boy  = input.nextDouble();
+
+        //Kilo (kg) / Boy(m) * Boy(m)= Vücut kütle indeksi
+        bmi = (kilo/(boy*boy));   //vücut kütle indeksi formülü
+        // sonuç:
+
+        System.out.println("Vücut Kütle İndeksiniz : "+ bmi);
     }
 }
